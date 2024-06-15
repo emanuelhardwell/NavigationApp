9c204381547c14ee0721c399068ccefee3114d34
emanuelhardwell <emanuelnoe16@gmail.com>
2024-06-15 15:19:19 -0600

    feat: Crate main route and install StackNavigator

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-import {Text} from 'react-native';
+import {StackNavigator} from './presentation/routes/StackNavigator';
 
 export const App = () => {
   return (
     <NavigationContainer>
-      <Text>App</Text>
+      <StackNavigator />
     </NavigationContainer>
   );
 };
