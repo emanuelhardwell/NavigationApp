7b74fc117c295ec07f2e2814015196518d30de37
emanuelhardwell <emanuelnoe16@gmail.com>
2024-06-18 13:18:25 -0600

    feat: Update the header of StackNavigator

--- src/presentation/routes/StackNavigator.tsx
+++ src/presentation/routes/StackNavigator.tsx
@@ -1,9 +1,10 @@
 import {createStackNavigator} from '@react-navigation/stack';
-import React from 'react';
 import {HomeScreen} from '../screens/home/HomeScreen';
 import {ProductsScreen} from '../screens/products/ProductsScreen';
 import {SettingsScreen} from '../screens/settings/SettingsScreen';
 import {ProductScreen} from '../screens/products/ProductScreen';
+import {useEffect} from 'react';
+import {useNavigation} from '@react-navigation/native';
 
 export type RootStackParams = {
   Home: undefined;
@@ -15,6 +16,14 @@ export type RootStackParams = {
 const Stack = createStackNavigator<RootStackParams>();
 
 export const StackNavigator = () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
     <Stack.Navigator
       screenOptions={{
