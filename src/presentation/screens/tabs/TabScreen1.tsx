9c39b2d4d5ae2a3c922c0197fcb8e5365608dfc8
emanuelhardwell <emanuelnoe16@gmail.com>
2024-06-17 23:24:46 -0600

    feat: Create template TabScreen and use the HamburgerMenu

---
+++ src/presentation/screens/tabs/TabScreen1.tsx
@@ -0,0 +1,11 @@
+import {Text, View} from 'react-native';
+import {HamburgerMenu} from '../../components/shared/HamburgerMenu';
+
+export const TabScreen1 = () => {
+  return (
+    <View>
+      <HamburgerMenu />
+      <Text>TabScreen1</Text>
+    </View>
+  );
+};
---
+++ src/presentation/screens/tabs/TabScreen3.tsx
@@ -0,0 +1,11 @@
+import {Text, View} from 'react-native';
+import {HamburgerMenu} from '../../components/shared/HamburgerMenu';
+
+export const TabScreen3 = () => {
+  return (
+    <View>
+      <HamburgerMenu />
+      <Text>TabScreen3</Text>
+    </View>
+  );
+};
