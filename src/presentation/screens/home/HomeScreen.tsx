64b271a6d7fe46b15d369785ab5ad1864c12455d
emanuelhardwell <emanuelnoe16@gmail.com>
2024-06-18 15:45:20 -0600

    fix: Use component HamburgerMenu and delete boilerplate

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,30 +1,16 @@
-import React, {useEffect} from 'react';
-import {Pressable, Text, View} from 'react-native';
+import {View} from 'react-native';
 import {globalStyles} from '../../theme/theme';
 import {ButtonPrimary} from '../../components/shared/ButtonPrimary';
-import {
-  DrawerActions,
-  NavigationProp,
-  useNavigation,
-} from '@react-navigation/native';
+import {NavigationProp, useNavigation} from '@react-navigation/native';
 import {RootStackParams} from '../../routes/StackNavigator';
+import {HamburgerMenu} from '../../components/shared/HamburgerMenu';
 
 export const HomeScreen = () => {
   const navigation = useNavigation<NavigationProp<RootStackParams>>();
 
-  useEffect(() => {
-    navigation.setOptions({
-      headerLeft: () => (
-        <Pressable
-          onPress={() => navigation.dispatch(DrawerActions.openDrawer)}>
-          <Text>Menu</Text>
-        </Pressable>
-      ),
-    });
-  }, []);
-
   return (
     <View style={globalStyles.container}>
+      <HamburgerMenu />
       <ButtonPrimary
         label="Products"
         onPress={() => navigation.navigate('Products')}
